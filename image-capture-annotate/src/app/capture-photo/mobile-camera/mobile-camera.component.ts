def9b5cb28faa4192a26980b0a552373d4b72d95
Gauri9 <gauriwalke99@gmail.com>
2023-12-12 12:27:12 +0530

    image rendering poc

--- image-capture-annotate/src/app/capture-photo/mobile-camera/mobile-camera.component.ts
+++ image-capture-annotate/src/app/capture-photo/mobile-camera/mobile-camera.component.ts
@@ -1,8 +1,4 @@
-
-
-import { Component, OnInit, DoCheck } from '@angular/core';
-import { BehaviorSubject, noop } from 'rxjs';
-import { finalize, map, tap } from 'rxjs/operators'
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mobile-camera',
@@ -10,67 +6,95 @@ import { finalize, map, tap } from 'rxjs/operators'
   styleUrls: ['./mobile-camera.component.scss']
 })
 export class MobileCameraComponent implements OnInit{
-  capturedImageUrl: string | undefined;
-  landscapeMode: boolean = false;
-  // deviceWidth: number = window.innerWidth;
-  // deviceHeight: number = window.innerHeight;
+  @ViewChild('imageElement') imageElement!: ElementRef<HTMLImageElement>;
+  @ViewChild('canvasElement') canvasElement!: ElementRef<HTMLCanvasElement>;
 
+  isEditing = false;
+  startX!: number;
+  startY!: number;
+
+  capturedImageUrl!: string;
+  landscapeMode: boolean = false;
 
   constructor() { }
 
-  ngOnInit() {
-    // window.addEventListener('resize', () => {
-    //   this.deviceWidth = window.innerWidth;
-    //   this.deviceHeight = window.innerHeight;
-    // });
+  ngOnInit() {}
 
+    ngAfterViewInit() {
+      if (this.imageElement && this.imageElement.nativeElement &&
+          this.canvasElement && this.canvasElement.nativeElement) {
+        // Set the canvas size to match the image size
+        const image = this.imageElement.nativeElement;
+        const canvas = this.canvasElement.nativeElement;
+
+        canvas.width = image.width;
+        canvas.height = image.height;
+        console.log('width || height',image.width, image.height)
+      } else {
+        console.error('Image or canvas element not found in ngAfterViewInit.');
+      }
+    }
+
+
+
+  startDrawing(event: TouchEvent) {
+    event.preventDefault();
+    console.log('start drawing...')
+    if (!this.isEditing) return;
+    const touch = event.touches[0];
+    this.startX = touch.clientX;
+    this.startY = touch.clientY;
+    console.log('co-ordinates:-',this.startX,this.startY)
+  }
+
+  touchMove(event: TouchEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log('touchmove');
+    if (!this.isEditing || this.startX === undefined || this.startY === undefined) return;
+
+    const ctx = this.canvasElement.nativeElement.getContext('2d');
+    if (!ctx) {
+      console.error('Could not get 2D rendering context.');
+      return;
+    }
+
+    console.log('ctx',ctx)
+    const touch = event.touches[0];
+    const width = touch.clientX - this.startX;
+    const height = touch.clientY - this.startY;
+
+    ctx.clearRect(0, 0, this.canvasElement.nativeElement.width, this.canvasElement.nativeElement.height);
+
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.strokeRect(this.startX, this.startY, width, height);
+  }
+
+
+  toggleEdit() {
+    this.isEditing = !this.isEditing;
   }
 
   
   takePhoto(photoSource: HTMLInputElement): void {
     console.log('inside take photo')
-    this.capturedImageUrl = 'inside take photo'
     if (photoSource.files && photoSource.files.length > 0) {
       const photoSourceObj = photoSource.files[0];
-      const reader = new FileReader();
-
-      // reader.onload = (e) => {
-      //   this.capturedImageUrl = 'inside onload'
-      //   this.capturedImageUrl = reader.result as string;
-      //   console.log('Captured Image URL:', this.capturedImageUrl);
-
-      //   // this.capturedImageUrl = imageUrl;
-
-      // };
+      const reader = new FileReader();    
 
       const handleEvent = (event: Event) => {
         this.capturedImageUrl = reader.result as string;
-        console.log('Captured Image URL:', this.capturedImageUrl);
-
-        const img = new Image();
-          img.src = this.capturedImageUrl;
-
-          if (!img.complete) {
-              img.onload = () => {
-                const aspectRatio = img.naturalWidth / img.naturalHeight;
-                console.log('Aspect Ratio:', aspectRatio);
-              };
-            }
-
       }
-      
       // Read the selected image file as a data URL
       reader.readAsDataURL(photoSourceObj);
       reader.addEventListener("load", handleEvent);
-      
     }
   }
 
   toggleLandscapeMode() {
     this.landscapeMode = !this.landscapeMode;
   }
-
-
 
 }
 
